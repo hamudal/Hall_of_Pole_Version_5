201ed67fa86297647baf5eeb3edb3b37e78dc1fd
Muhammed Al-Dalqamouni <hamudal.hd@gmail.com>
2023-12-20 17:53:31 +0100

    Fixed Button Mapping; MainFrame works with 1 URL, missing error handlers

--- Scraper/Scraper Function without prop Docu/Scraper Function Docu/a_URL_Reconstructor.py
+++ Scraper/Scraper Function without prop Docu/Scraper Function Docu/a_URL_Reconstructor.py
@@ -16,6 +16,9 @@ def reconstruct_urls_and_extract_buttons(url):
         'Team': 's/team'
     }
 
+    # Extrahieren des dynamischen Teils der URL
+    dynamic_part = url.split("/")[-1]
+
     link_text = []
     reconstructed_urls = {}
 
@@ -25,7 +28,7 @@ def reconstruct_urls_and_extract_buttons(url):
             text = a.text
             link_text.append(text)
             if text in button_url_mapping:
-                reconstructed_url = f"https://www.eversports.de/{button_url_mapping[text]}/poda-studio"
+                reconstructed_url = f"https://www.eversports.de/{button_url_mapping[text]}/{dynamic_part}"
                 reconstructed_urls[text] = reconstructed_url
 
     return link_text, reconstructed_urls
@@ -34,8 +37,3 @@ def reconstruct_urls_and_extract_buttons(url):
 # url = "https://www.eversports.de/s/poda-studio"
 # link_text, reconstructed_urls = reconstruct_urls_and_extract_buttons(url)
 # print(link_text, reconstructed_urls)
-
-
-
-
-### FIX reconstructed_url = f"https://www.eversports.de/{button_url_mapping[text]}/poda-studio"
