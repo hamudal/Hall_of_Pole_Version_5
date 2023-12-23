4abb0115cd47910dfca1b287481fb0d454b82e9d
Muhammed Al-Dalqamouni <hamudal.hd@gmail.com>
2023-12-23 22:25:31 +0100

    All_Found URL + Righht Videos + Team

--- 1_Latest_version_Hop_Scrapper_V5/Scraper_V3/a_URLS_Reconstruction.py
+++ 1_Latest_version_Hop_Scrapper_V5/Scraper_V3/a_URLS_Reconstruction.py
@@ -2,18 +2,24 @@ import requests
 from bs4 import BeautifulSoup
 
 def get_response_content(url):
-    """Holt den Inhalt einer Webseite über eine HTTP-Anfrage."""
+    """ Retrieves the content of a webpage via an HTTP request. """
     try:
         response = requests.get(url)
         response.raise_for_status()
         return response.content
     except requests.RequestException as e:
-        print(f"Fehler bei der HTTP-Anfrage: {e}")
+        print(f"Error during HTTP request: {e}")
         return None
 
 def reconstruct_urls_and_extract_buttons(url):
     """
-    Rekonstruiert URLs und extrahiert Buttons für eine gegebene URL.
+    Reconstructs URLs and extracts buttons for a given URL.
+
+    Args:
+        url (str): The URL of the webpage to scrape.
+
+    Returns:
+        tuple: A list of link texts and a dictionary of reconstructed URLs.
     """
     content = get_response_content(url)
     if content is None:
@@ -26,9 +32,9 @@ def reconstruct_urls_and_extract_buttons(url):
         'Übersicht': 's',
         'Klassen': 'scl',
         'Workshops': 'sw',
-        'Videos': 's/videos',
+        'Videos': 's',
         'Preise': 'sp',
-        'Team': 's/team'
+        'Team': 's'
     }
 
     dynamic_part = url.split("/")[-1]
@@ -41,16 +47,18 @@ def reconstruct_urls_and_extract_buttons(url):
             for anchor in anchor_elements:
                 text = anchor.text
                 link_text.append(text)
-                if text in button_url_mapping:
+                if text in ['Videos', 'Team']:
+                    reconstructed_url = f"{url}/{text.lower()}"
+                elif text in button_url_mapping:
                     reconstructed_url = f"https://www.eversports.de/{button_url_mapping[text]}/{dynamic_part}"
                 else:
-                    # Hier wird der Original-Text in der URL gespeichert, falls er nicht im Mapping ist
-                    reconstructed_url = f"{url}/{text.lower()}"
+                    # Handling unrecognized text
+                    reconstructed_url = f"{url}/{text.lower().replace(' ', '-')}"
                 reconstructed_urls[text] = reconstructed_url
 
     return link_text, reconstructed_urls
 
-# Beispielaufruf der Funktion
-# url = "https://www.eversports.de/s/poda-studio"
-# link_text, reconstructed_urls = reconstruct_urls_and_extract_buttons(url)
-# print(link_text, reconstructed_urls)
+# Example call of the function
+url = "https://www.eversports.de/s/poda-studio"
+link_text, reconstructed_urls = reconstruct_urls_and_extract_buttons(url)
+print(link_text, reconstructed_urls)
